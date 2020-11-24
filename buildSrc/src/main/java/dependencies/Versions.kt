199268f4511b639418e97ea5e682ed935c836c7b
nima abdpoor <nimaabdi328@yahoo.com>
2020-11-24 15:40:09 +0330

    gradle dependencies completed.

--- buildSrc/src/main/java/dependencies/Versions.kt
+++ buildSrc/src/main/java/dependencies/Versions.kt
@@ -1,11 +1,11 @@
 package dependencies
 
 object Versions {
-    val gradle = "3.5.3"
+    val gradle = "4.0.1"
     val compilesdk = 29
     val minsdk = 21
     val targetsdk = 29
-    val kotlin = "1.3.61"
+    val kotlin = "1.4.10"
     val ktx = "1.2.0"
     val dagger = "2.25.4"
     val nav_components = "2.3.0-alpha06"
