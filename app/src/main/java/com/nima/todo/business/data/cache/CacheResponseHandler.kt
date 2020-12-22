c9eb87f8d913df3cfe7039e6fc5f94ee1feff4b8
nima abdpoor <nimaabdi328@yahoo.com>
2020-12-22 10:54:41 +0330

    data layer cache

--- app/src/main/java/com/nima/todo/business/data/cache/CacheResponseHandler.kt
+++ app/src/main/java/com/nima/todo/business/data/cache/CacheResponseHandler.kt
@@ -1,13 +1,45 @@
 package com.nima.todo.business.data.cache
 
-import com.nima.todo.business.domain.state.DataState
-import com.nima.todo.business.domain.state.StateEvent
+import com.nima.todo.business.data.cache.CacheErrors.CACHE_DATA_NULL
+import com.nima.todo.business.domain.state.*
 
-abstract class CacheResponseHandler<ViewState , Data>(
+abstract class CacheResponseHandler<ViewState , Data>(  
     private val response : CacheResult<Data?> ,
     private val stateEvent: StateEvent?
 ){
     suspend fun getResult()  :DataState<ViewState>?{
+        return when(response){
+            is CacheResult.Error ->errorHandling(response)
+            is CacheResult.Success -> successHandling(response)
+        }
+    }
 
+    private fun errorHandling(response: CacheResult.Error): DataState<ViewState>? {
+        return DataState.error(
+            response = Response(
+                message = "${stateEvent?.errorInfo()}\n\n Reason : ${response.errorMessage}",
+                uiComponentType = UIComponentType.Dialog(),
+                messageType = MessageType.Error()
+            ),
+            stateEvent =stateEvent
+        )
+    }
+
+     abstract fun successHandling(response: CacheResult.Success<Data?>): DataState<ViewState>? {
+         response.value?:let {
+            return DataState.error(
+                response = Response(
+                    message = "${stateEvent?.errorInfo()}\n\n Reason : $CACHE_DATA_NULL",
+                    uiComponentType = UIComponentType.Dialog(),
+                    messageType = MessageType.Error()
+                ),
+                stateEvent =stateEvent
+            )
+        }
+        return response.value?.let {
+            DataState.data(
+                response = Response(),
+            )
+        }
     }
 }
