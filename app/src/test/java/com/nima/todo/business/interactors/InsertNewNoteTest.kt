ad7a830ea9fbc22134521883941d3ae99e3b038f
nima-abdpoor <nimaabdi328@yahoo.com>
2021-06-06 09:54:38 +0430

    creating class for test the InsertingNewNotes.

--- app/src/test/java/com/nima/todo/business/interactors/InsertNewNoteTest.kt
+++ app/src/test/java/com/nima/todo/business/interactors/InsertNewNoteTest.kt
@@ -1,4 +1,29 @@
 package com.nima.todo.business.interactors
 
+import com.nima.todo.business.data.cache.abstraction.NoteCacheDataSource
+import com.nima.todo.business.data.network.abstraction.NoteNetworkDataSource
+import com.nima.todo.business.domain.model.NoteFactory
+import com.nima.todo.business.interactors.notelist.InsertNewNotes
+import com.nima.todo.di.DependencyContainer
+
 class InsertNewNoteTest {
+    private val insertNewNote: InsertNewNotes
+
+    // dependencies
+    private val dependencyContainer: DependencyContainer = DependencyContainer()
+    private val noteCacheDataSource: NoteCacheDataSource
+    private val noteNetworkDataSource: NoteNetworkDataSource
+    private val noteFactory: NoteFactory
+
+    init {
+        dependencyContainer.build()
+        noteCacheDataSource = dependencyContainer.noteCacheDataSource
+        noteNetworkDataSource = dependencyContainer.noteNetworkDataSource
+        noteFactory = dependencyContainer.noteFactory
+        insertNewNote = InsertNewNotes(
+            noteCacheDataSource = noteCacheDataSource,
+            noteNetworkDataSource = noteNetworkDataSource,
+            noteFactory = noteFactory
+        )
+    }
 }
